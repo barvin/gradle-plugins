61037a1aace23780caeafbefa54a9d1db99709ad
Maksym Barvinskyi <Maksym_Barvinskyi@epam.com>
2019-07-15 10:14:31 +0300

    Imports clean up

--- reminders-app/src/main/java/com/example/sut/SystemUnderTestApplication.java
+++ reminders-app/src/main/java/com/example/sut/SystemUnderTestApplication.java
@@ -1,12 +1,7 @@
 package com.example.sut;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class SystemUnderTestApplication {
