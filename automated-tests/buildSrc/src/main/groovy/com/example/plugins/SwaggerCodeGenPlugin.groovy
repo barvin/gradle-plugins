eca85f88ebfe6df53dcceab5183e502736e39f74
Maksym Barvinskyi <Maksym_Barvinskyi@epam.com>
2019-07-15 16:46:01 +0300

    Applied JavaParser

--- automated-tests/buildSrc/src/main/groovy/com/example/plugins/SwaggerCodeGenPlugin.groovy
+++ automated-tests/buildSrc/src/main/groovy/com/example/plugins/SwaggerCodeGenPlugin.groovy
@@ -1,10 +1,14 @@
-package com.example.plugins
+package com.example.plugins;
 
-import org.gradle.api.Plugin
-import org.gradle.api.Project
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
 
 class SwaggerCodeGenPlugin implements Plugin<Project> {
     void apply(Project project) {
-
+        project.task('generateApi', {
+            doLast {
+                println('bbbbbbbbbbbbbbbbb')
+            }
+        })
     }
 }
