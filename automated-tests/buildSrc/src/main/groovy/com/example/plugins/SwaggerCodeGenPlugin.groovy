8c1f76849b6c2db7aecd6296fd65b0b913745d84
Maksym Barvinskyi <Maksym_Barvinskyi@epam.com>
2019-07-16 00:09:21 +0300

    Finished TestFormatCheck plugin

--- automated-tests/buildSrc/src/main/groovy/com/example/plugins/SwaggerCodeGenPlugin.groovy
+++ automated-tests/buildSrc/src/main/groovy/com/example/plugins/SwaggerCodeGenPlugin.groovy
@@ -1,14 +1,46 @@
-package com.example.plugins;
+package com.example.plugins
 
-import org.gradle.api.Plugin;
-import org.gradle.api.Project;
+import io.swagger.codegen.DefaultGenerator
+import io.swagger.codegen.config.CodegenConfigurator
+import org.gradle.api.Plugin
+import org.gradle.api.Project
 
 class SwaggerCodeGenPlugin implements Plugin<Project> {
+    private static final String SWAGGER_JSON_URL = 'http://localhost:8080/v2/api-docs'
+    private static final String INVOKER_PACKAGE = 'com.example.services.api.client'
+    private static final String API_PACKAGE = 'com.example.services.api.controllers'
+    private static final String MODEL_PACKAGE = 'com.example.entities.generated'
+
     void apply(Project project) {
         project.task('generateApi', {
             doLast {
-                println('bbbbbbbbbbbbbbbbb')
+                new File("$project.projectDir/temp").deleteDir()
+                deletePackageDirectories(project.projectDir.toString(), [INVOKER_PACKAGE, API_PACKAGE, MODEL_PACKAGE])
+
+                def config = new CodegenConfigurator()
+                config.inputSpec = SWAGGER_JSON_URL
+                config.outputDir = "$project.projectDir/temp"
+                config.lang = 'java'
+                config.library = 'rest-assured'
+                config.additionalProperties = [
+                    "invokerPackage"         : INVOKER_PACKAGE,
+                    "apiPackage"             : API_PACKAGE,
+                    "modelPackage"           : MODEL_PACKAGE,
+                    "dateLibrary"            : "java8",
+                    "hideGenerationTimestamp": "true"
+                ]
+                new DefaultGenerator().opts(config.toClientOptInput()).generate()
+
+                new AntBuilder().copy(todir: "$project.projectDir/src/test/java") {
+                    fileset(dir: "$project.projectDir/temp/src/main/java")
+                }
             }
         })
+    }
+
+    static void deletePackageDirectories(String projectPath, List<String> packages) {
+        packages.forEach { pack ->
+            new File("$projectPath/src/test/java/${pack.replace('.', '/')}").deleteDir()
+        }
     }
 }
